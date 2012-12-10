cf09d61ba6d3331ce10eb445ff752e1cfc9ef306
Andrew Yavtushenko <a.yavtushenko@111minutes.com>
2012-12-10 17:59:15 +0200

    regex from maniac

--- upload.js
+++ upload.js
@@ -1,7 +1,7 @@
 function load_image_previews (input, previewsWraper) {
   var previews = [],
       readers = [],
-      rFilter = /^(?:image\/bmp|image\/cis\-cod|image\/gif|image\/ief|image\/jpeg|image\/jpeg|image\/jpeg|image\/pipeg|image\/png|image\/svg\+xml|image\/tiff|image\/x\-cmu\-raster|image\/x\-cmx|image\/x\-icon|image\/x\-portable\-anymap|image\/x\-portable\-bitmap|image\/x\-portable\-graymap|image\/x\-portable\-pixmap|image\/x\-rgb|image\/x\-xbitmap|image\/x\-xpixmap|image\/x\-xwindowdump)$/i;
+      rFilter = /^image\/(?:png|jpe?g|gif)$/;
 
   function build_previews (source) {
     var li = document.createElement("li");
